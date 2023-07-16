d1628781ca78e95c599ae4bd5e96c329e098a9cc
Pedro Arrieta <pedro.arrieta@ad.grupopcr.com.pa>
2023-07-16 17:02:01 -0500

    procesamiento de imagnes

--- php/prueba.php
+++ php/prueba.php
@@ -1,22 +1,58 @@
-<?php 
+<?php
+$apiUrl = 'https://brazilsouth.api.cognitive.microsoft.com/vision/v2.0/ocr';
+$apiKey = '90bc4d76077a4a6e9eb5ce2fbf2941ec';
 
-require_once 'vendor/autoload.php';
-use thiagoalessio\TesseractOCR\TesseractOCR;
-$image = 'cedula.jpg';
-$imageResource = imagecreatefromjpeg($image);
-imagefilter($imageResource, IMG_FILTER_GRAYSCALE);
-imagefilter($imageResource, IMG_FILTER_CONTRAST, -15);
-$tempImage = 'temp.jpg';
-imagejpeg($imageResource, $tempImage);
-$ocr = new TesseractOCR($tempImage);
-$ocr->lang('spa');
-$ocr->osd();
-$text = $ocr->run();
+$imageUrl = 'http://ctc.grupopcr.com.pa/material/pasaporte/1.PNG';
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\nOcp-Apim-Subscription-Key: $apiKey\r\n",
+        'method'  => 'POST',
+        'content' => json_encode(array(
+            'url' => $imageUrl,
+        )),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($apiUrl, false, $context);
+
+if ($result === FALSE) {
+    /* Handle error */
+}
+
+// Parsea el resultado
+$resultData = json_decode($result, true);
+
+// Extrae el texto de cada región
+$text = '';
+foreach ($resultData['regions'] as $region) {
+    foreach ($region['lines'] as $line) {
+        foreach ($line['words'] as $word) {
+            $text .= $word['text'] . ' ';
+        }
+        $text .= "\n";
+    }
+    $text .= "\n";
+}
+
+// Muestra el texto
 echo $text;
-preg_match('/([A-Z]{0,2}-\d{1,3}-\d{3,4})/', $text, $idMatches);
-$id = $idMatches[1] ?? '';
-echo "ID: $id <br>";
-// Eliminar la imagen temporal
-unlink($tempImage);
+echo '<pre>';
+var_dump($text);
+echo '</pre>';
+
+echo '///////////////////////////////////////////////////////';
+
+$array = explode(" ", $text);
+
+echo '<pre>';
+var_dump($array);
+echo '</pre>';
+
+echo '///////////////////////////////////////////////////////';
+
+echo 'Nombre: '.$array[5].' '.$array[6].' '.$array[7].' <br>';
+echo 'Fecha de nacimineto: ' .$array[13].' <br>';
+echo 'Numero de identificacion : ' .$array[28]
 
 ?>
